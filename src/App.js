520823c5f2f6146255ca01eff6231061bb937c57
Nathan Ahn <nathancahn@gmail.com>
2020-11-07 17:47:22 -0500

    Added extra pages and page routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, { Component } from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
+import CreateEvent from "./pages/CreateEvent"
+import SignIn from "./pages/SignIn"
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Hi
-      </p>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+
+          <Switch>
+            <Route path="/" exact>Home</Route>
+            <Route path="/signin" exact>SignIn</Route>
+            <Route>Page not found</Route>
+          </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
